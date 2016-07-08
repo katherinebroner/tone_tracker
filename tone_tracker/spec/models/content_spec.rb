498353e2807e3a1d406825749b2a2345256e6311
Katherine Broner <katherine.broner@gmail.com>
2016-07-08 18:24:26 -0400

    validation test - content

--- tone_tracker/spec/models/content_spec.rb
+++ tone_tracker/spec/models/content_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Content, type: :model do
+describe Content do
+  let(:content) { Content.create(body: Faker::Lorem.paragraph) }
+
+  describe "#validations" do
+    it "is a valid Content object" do
+      expect(content).to be_valid
+    end
+
+    let(:invalid_content) { Content.create(body: nil) }
+    it "is an invalid Content object" do
+      expect(invalid_content).not_to be_valid
+    end
+  end
 end
