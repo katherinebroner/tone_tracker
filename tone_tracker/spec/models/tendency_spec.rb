91136036b822a7c4bb9115781c7aa5262724c415
Katherine Broner <katherine.broner@gmail.com>
2016-07-09 14:20:02 -0400

    validation test - tendency

--- tone_tracker/spec/models/tendency_spec.rb
+++ tone_tracker/spec/models/tendency_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Tendency, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Tendency, type: :model do
+describe Tendency do
+  let(:tendency) { Tendency.create(name: Faker::Lorem.word, score: 1.0)}
+
+  describe "#validations" do
+    it "is a valid Tendency object" do
+      expect(tendency).to be_valid
+    end
+
+    let(:invalid_tendency) { Tendency.create(name: nil, score: nil)}
+    it "is an invalid Tendency object" do
+      expect(invalid_tendency).not_to be_valid
+    end
+
+    let(:invalid_tendency) { Tendency.create(name: nil, score: 1.0)}
+    it "is an invalid Tendency object" do
+      expect(invalid_tendency).not_to be_valid
+    end
+
+    let(:invalid_tendency) { Tendency.create(name: Faker::Lorem.word, score: nil)}
+    it "is an invalid Tendency object" do
+      expect(invalid_tendency).not_to be_valid
+    end
+  end
 end
