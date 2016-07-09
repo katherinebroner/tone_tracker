7c5953cc9bcf646f1fe7ff5d74c7875a3879f9ea
Katherine Broner <katherine.broner@gmail.com>
2016-07-09 14:07:37 -0400

    validation test - language

--- tone_tracker/spec/models/language_spec.rb
+++ tone_tracker/spec/models/language_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 # RSpec.describe Language, type: :model do
+describe Language do
+  let(:language) { Language.create(name: Faker::Lorem.word, score: 1.0)}
 
+  describe "#validations" do
+    it "is a valid Language object" do
+      expect(language).to be_valid
+    end
+
+    let(:invalid_language) { Language.create(name: nil, score: nil)}
+    it "is an invalid Language object" do
+      expect(invalid_language).not_to be_valid
+    end
+
+    let(:invalid_language) { Language.create( name: nil, score: 1.0)}
+    it "is an invalid Language object" do
+      expect(invalid_language).not_to be_valid
+    end
+
+    let(:invalid_language) { Language.create( name: Faker::Lorem.word, score: nil)}
+    it "is an invalid Language object" do
+      expect(invalid_language).not_to be_valid
+    end
+  end
 
 end
