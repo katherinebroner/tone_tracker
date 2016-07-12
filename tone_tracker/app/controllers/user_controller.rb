568c7ff69d0483b73bcc6ea284a62f5098859975
Katherine Broner <katherine.broner@gmail.com>
2016-07-12 15:57:53 -0400

    new user method - user controller

---
+++ tone_tracker/app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
