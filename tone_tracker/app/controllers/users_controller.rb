e9c111ec161b6e7a630cb6b258f278cfa4fe1ca1
Katherine Broner <katherine.broner@gmail.com>
2016-07-12 16:31:54 -0400

    show user method - user controller

--- tone_tracker/app/controllers/users_controller.rb
+++ tone_tracker/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     if logged_in?
       redirect_to root_path
@@ -18,9 +19,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    if logged_in?
+      @user = current_user
+    else
+      redirect_to root_path
+    end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
