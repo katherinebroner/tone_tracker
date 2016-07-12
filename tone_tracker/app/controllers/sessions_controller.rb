e317f3b927572f61cba36e93e88b6542b0002b7b
Katherine Broner <katherine.broner@gmail.com>
2016-07-12 17:19:28 -0400

    sessions create method

--- tone_tracker/app/controllers/sessions_controller.rb
+++ tone_tracker/app/controllers/sessions_controller.rb
@@ -9,8 +9,30 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(login_username)
+    if @user && @user.authenticate(login_password[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      if @user
+        @user.errors[:password].push("incorrect")
+      else
+        @user = User.new
+        @user.errors[:username].push("not found")
+      end
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+  def login_username
+    params.require(:login).permit(:username)
+  end
+
+  def login_password
+    params.require(:login).permit(:password)
   end
 end
