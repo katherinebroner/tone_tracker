30dea88a69130824b685c5defe61182913f3212e
Katherine Broner <katherine.broner@gmail.com>
2016-07-13 12:18:24 -0400

    POST #create valid params controller test - user

--- tone_tracker/spec/controllers/user_controller_spec.rb
+++ tone_tracker/spec/controllers/user_controller_spec.rb
@@ -30,4 +30,34 @@ describe UsersController do
       end
     end
   end
+
+  describe "POST #create" do
+
+    context "when valid params are passed" do
+
+    before :each do
+      post :create, { user: {
+        username: Faker::Internet.user_name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password } }
+    end
+
+      it "creates a new User" do
+        expect {
+          post :create, { user: {
+          username: Faker::Internet.user_name,
+          email: Faker::Internet.email,
+          password: Faker::Internet.password } }
+        }.to change(User, :count).by(1)
+      end
+
+      it "assigns a newly created User as @user" do
+        expect(assigns(:user)).to be_an_instance_of(User)
+      end
+
+      it "redirects to the home page" do
+        expect(subject).to redirect_to(root_path)
+      end
+    end
+  end
 end
